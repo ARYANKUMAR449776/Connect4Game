4d9fb42a17398ac3fbf9d30a25fbebd5676d386a
ARYANKUMAR449776 <aimpendance@gmail.com>
2024-03-26 11:59:50 -0600

    connect4 structure

---
+++ Coonect4FinalProject/Coonect4FinalProject/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System.ComponentModel.Design;
+using System.Security.Claims;
+/*   Class designed to play the game  */
+class Controller
+{
+
+}
+/*    Class designed for keeping track of the turns    */
+class Model
+{
+
+}
+/*    Class Designed for player 1    */
+class Player1
+{
+
+}
+/*    Class Designed for player 2    */
+class Player2
+{
+
+}
+
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("");
+    }
+}
